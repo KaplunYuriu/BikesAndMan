d20c56bf40d358d408c9bf88ea14564848748b73
KaplunYuriy <kaplun.yuriy@gmail.com>
2018-04-05 15:57:20 +0300

    Display all data

--- BikesAndMen/Program.fs
+++ BikesAndMen/Program.fs
@@ -1,7 +1,19 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+open FSharp.Data;
+open FSharp.Charting;
+open System;
+
+module public DayDataSet = 
+    type CsvData = CsvProvider<"C:/code/BikesAndMen/BikesAndMen/Dataset/day.csv">
+    let data = CsvData.Load("C:/code/BikesAndMen/BikesAndMen/Dataset/day.csv").Rows
+    
+
+
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    let all = Chart.Line([ for obs in DayDataSet.data -> obs.Cnt ]).ShowChart()
+    System.Windows.Forms.Application.Run(all)
+    let value = Console.ReadLine()
     0 // return an integer exit code
