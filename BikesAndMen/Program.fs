79fc54d0b58b550aba77890484f9b5ed414a4e38
KaplunYuriy <kaplun.yuriy@gmail.com>
2018-04-05 16:12:22 +0300

    Display average by week and month

--- BikesAndMen/Program.fs
+++ BikesAndMen/Program.fs
@@ -5,15 +5,26 @@ open FSharp.Data;
 open FSharp.Charting;
 open System;
 
-module public DayDataSet = 
-    type CsvData = CsvProvider<"C:/code/BikesAndMen/BikesAndMen/Dataset/day.csv">
-    let data = CsvData.Load("C:/code/BikesAndMen/BikesAndMen/Dataset/day.csv").Rows
-    
+type CsvData = CsvProvider<"C:/code/BikesAndMen/BikesAndMen/Dataset/day.csv">
 
+let data = CsvData.Load("C:/code/BikesAndMen/BikesAndMen/Dataset/day.csv").Rows
+    
+let ma n (series: float seq) =
+    series
+    |> Seq.windowed n
+    |> Seq.map (fun xs -> xs |> Seq.average)
+    |> Seq.toList
 
 [<EntryPoint>]
 let main argv = 
-    let all = Chart.Line([ for obs in DayDataSet.data -> obs.Cnt ]).ShowChart()
-    System.Windows.Forms.Application.Run(all)
+    let sev = ma 7 [for obs in data -> (float)obs.Cnt]
+
+    let combined = Chart.Combine[
+        Chart.Line [for obs in data -> obs.Cnt]
+        Chart.Line (ma 7 [for obs in data -> (float)obs.Cnt])
+        Chart.Line (ma 30 [for obs in data -> (float)obs.Cnt])
+    ]
+
+    System.Windows.Forms.Application.Run(combined.ShowChart())
     let value = Console.ReadLine()
     0 // return an integer exit code
